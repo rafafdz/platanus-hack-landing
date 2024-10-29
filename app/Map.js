12d245bce83cc244c551816c2a96029dbafe9fad
Rafael Fernández <rafafdz01@gmail.com>
2024-10-29 00:58:27 -0600

    feat: dark mode map

--- app/Map.js
+++ app/Map.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
+import styles from './Map.module.css';
 import L from 'leaflet';
 
 delete L.Icon.Default.prototype._getIconUrl;
@@ -13,7 +14,11 @@ L.Icon.Default.mergeOptions({
 
 const Map = ({ position }) => {
   return (
-    <MapContainer center={position} zoom={15} style={{ height: '100%', width: '100%' }} className="z-10">
+    <MapContainer 
+      center={position} 
+      zoom={15} 
+      className={styles.mapContainer}
+    >
       <TileLayer
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
