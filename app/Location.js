a2ed5ba39d23efc42be12342718f819e758add20
Rafael Fernández <rafafdz01@gmail.com>
2024-11-06 23:55:10 -0300

    feat(sponsors): add soyio

--- app/Location.js
+++ app/Location.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import Image from 'next/image';
 import dynamic from 'next/dynamic';
 import Tilt from './Tilt';
+import Padding from './Padding';
 import { FaWaze, FaPlay, FaStop, FaMapMarkerAlt } from 'react-icons/fa';
 import { SiGooglemaps } from 'react-icons/si';
 import TypewriterTitle from './TypewriterTitle';
@@ -16,88 +17,92 @@ const Location = () => {
   const [isHovered, setIsHovered] = useState(false);
 
   return (
-    <section className="min-h-screen flex flex-col items-center justify-center p-4">
-      <TypewriterTitle text="$ curl ipinfo.io/geo" className="font-oxanium text-2xl md:text-5xl font-bold mb-12 text-center" />
-      
-      <Tilt
-        className="parallax-effect w-full md:w-[34rem]"
-        tiltMaxAngleX={5}
-        tiltMaxAngleY={5}
-        perspective={1000}
-        transitionSpeed={1500}
-        scale={1.02}
-        gyroscope={true}
-      >
-        <div 
-          className="border border-zinc-400 rounded-lg overflow-hidden mb-8 transition-all duration-300 hover:border-primary w-full"
-          onMouseEnter={() => setIsHovered(true)}
-          onMouseLeave={() => setIsHovered(false)}
-        >
-          <div className="flex flex-col md:flex-row">
-            <div className={`md:w-1/3 relative h-64 md:h-auto transition-all duration-300 ${isHovered ? '' : 'grayscale'}`}>
-              <Image
-                src="/oficina-fintual-resize.jpg"
-                alt="Event Location"
-                fill
-                objectFit="cover"
-              />
-            </div>
-            <div className="md:w-2/3 p-4 flex flex-col justify-between bg-transparent">
-              <div>
-                <h2 className={`text-2xl font-bold mb-2 ${isHovered ? 'text-primary' : 'text-zinc-300'}`}>platanus hack</h2>
+    <section className="min-h-screen flex flex-col items-center justify-center">
+      <Padding>
+        <div className="flex flex-col items-center">
+          <TypewriterTitle text="$ curl ipinfo.io/geo" className="font-oxanium text-2xl md:text-5xl font-bold mb-12 text-center" />
+
+          <Tilt
+            className="parallax-effect w-full md:w-[34rem]"
+            tiltMaxAngleX={5}
+            tiltMaxAngleY={5}
+            perspective={1000}
+            transitionSpeed={1500}
+            scale={1.02}
+            gyroscope={true}
+          >
+            <div
+              className="border border-zinc-400 rounded-lg overflow-hidden mb-8 transition-all duration-300 hover:border-primary w-full"
+              onMouseEnter={() => setIsHovered(true)}
+              onMouseLeave={() => setIsHovered(false)}
+            >
+              <div className="flex flex-col md:flex-row">
+                <div className={`md:w-1/3 relative h-64 md:h-auto transition-all duration-300 ${isHovered ? '' : 'grayscale'}`}>
+                  <Image
+                    src="/oficina-fintual-resize.jpg"
+                    alt="Event Location"
+                    fill
+                    objectFit="cover"
+                  />
+                </div>
+                <div className="md:w-2/3 p-4 flex flex-col justify-between bg-transparent">
+                  <div>
+                    <h2 className={`text-2xl font-bold mb-2 ${isHovered ? 'text-primary' : 'text-zinc-300'}`}>platanus hack</h2>
+                  </div>
+                  <div className="space-y-2">
+                    <div className={`flex items-center ${isHovered ? 'text-primary' : 'text-zinc-300'}`}>
+                      <FaPlay className="mr-2" />
+                      <span>2024-11-22T18:45:00-03:00</span>
+                    </div>
+                    <div className={`flex items-center ${isHovered ? 'text-primary' : 'text-zinc-300'}`}>
+                      <FaStop className="mr-2" />
+                      <span>2024-11-24T17:00:00-03:00</span>
+                    </div>
+                    <div className={`flex items-center ${isHovered ? 'text-primary' : 'text-zinc-300'}`}>
+                      <FaMapMarkerAlt className="mr-2" />
+                      <span>fintual hq, providencia 229 🇨🇱</span>
+                    </div>
+                  </div>
+                </div>
               </div>
-              <div className="space-y-2">
-                <div className={`flex items-center ${isHovered ? 'text-primary' : 'text-zinc-300'}`}>
-                  <FaPlay className="mr-2" />
-                  <span>2024-11-22T18:45:00-03:00</span>
-                </div>
-                <div className={`flex items-center ${isHovered ? 'text-primary' : 'text-zinc-300'}`}>
-                  <FaStop className="mr-2" />
-                  <span>2024-11-24T17:00:00-03:00</span>
-                </div>
-                <div className={`flex items-center ${isHovered ? 'text-primary' : 'text-zinc-300'}`}>
-                  <FaMapMarkerAlt className="mr-2" />
-                  <span>fintual hq, providencia 229 🇨🇱</span>
+            </div>
+          </Tilt>
+
+          <div className="flex items-center justify-center w-full">
+            <Tilt
+              className="parallax-effect w-full h-96 md:size-[34rem] max-w-full"
+              tiltMaxAngleX={5}
+              tiltMaxAngleY={5}
+              perspective={1000}
+              transitionSpeed={1500}
+              scale={1.05}
+              gyroscope={true}
+            >
+              <div className="rounded-lg shadow-2xl overflow-hidden relative size-full">
+                <DynamicMap position={position} />
+                <div className="absolute bottom-4 left-4 flex space-x-2 z-30">
+                  <a
+                    href="https://www.google.com/maps/search/?api=1&query=Providencia+229,+Santiago,+Chile"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="p-2 rounded-full shadow-md hover:bg-zinc-700 transition duration-300 bg-zinc-950"
+                  >
+                    <SiGooglemaps className="w-6 h-6 text-primary" />
+                  </a>
+                  <a
+                    href="https://www.waze.com/ul?ll=-33.4267,-70.6167&navigate=yes"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="p-2 rounded-full shadow-md hover:bg-zinc-700 transition duration-300 bg-zinc-950"
+                  >
+                    <FaWaze className="w-6 h-6 text-primary" />
+                  </a>
                 </div>
               </div>
-            </div>
+            </Tilt>
           </div>
         </div>
-      </Tilt>
-
-      <div className="flex items-center justify-center w-full">
-        <Tilt
-          className="parallax-effect w-full h-96 md:size-[34rem] max-w-full"
-          tiltMaxAngleX={5}
-          tiltMaxAngleY={5}
-          perspective={1000}
-          transitionSpeed={1500}
-          scale={1.05}
-          gyroscope={true}
-        >
-          <div className="rounded-lg shadow-2xl overflow-hidden relative size-full">
-            <DynamicMap position={position} />
-            <div className="absolute bottom-4 left-4 flex space-x-2 z-30">
-              <a
-                href="https://www.google.com/maps/search/?api=1&query=Providencia+229,+Santiago,+Chile"
-                target="_blank"
-                rel="noopener noreferrer"
-                className="p-2 rounded-full shadow-md hover:bg-zinc-700 transition duration-300 bg-zinc-950"
-              >
-                <SiGooglemaps className="w-6 h-6 text-primary" />
-              </a>
-              <a
-                href="https://www.waze.com/ul?ll=-33.4267,-70.6167&navigate=yes"
-                target="_blank"
-                rel="noopener noreferrer"
-                className="p-2 rounded-full shadow-md hover:bg-zinc-700 transition duration-300 bg-zinc-950"
-              >
-                <FaWaze className="w-6 h-6 text-primary" />
-              </a>
-            </div>
-          </div>
-        </Tilt>
-      </div>
+      </Padding>
     </section>
   );
 };
--- app/Sponsors.js
+++ app/Sponsors.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Image from 'next/image';
 import TypewriterTitle from './TypewriterTitle';
+import Padding from './Padding';
 import Tilt from './Tilt';
 
 const Sponsors = () => {
@@ -8,56 +9,60 @@ const Sponsors = () => {
     { src: 'fintual-logo-white.png', size: 400, alt: 'Fintual logo', url: 'https://fintual.cl/' },
     { src: 'aws-logo-white.png', size: 200, alt: 'Amazon Web Services logo', url: 'https://aws.amazon.com/' }
   ];
-  
+
   const sponsorLogosSecondary = [
     { src: 'buk-logo-white.png', size: 170, alt: 'Buk logo', url: 'https://buk.cl/' },
   ];
-  
+
   const sponsorLogosTertiary = [
     { src: 'shinkansen-logo-white.svg', size: 205, alt: 'Shinkansen logo', url: 'https://shinkansen.finance/' },
     { src: 'pullpo-logo-white.svg', size: 125, alt: 'Pullpo logo', url: 'https://pullpo.io/' },
     { src: 'fingo-logo-white.svg', size: 125, alt: 'Fingo logo', url: 'https://fingo.cl/' },
     { src: 'oragus-logo-white.svg', size: 125, alt: 'Oragus logo', url: 'https://oragus.com/' },
+    { src: 'soyio-logo-white.png', size: 140, alt: 'Soyio logo', url: 'https://soyio.id/', className: 'grayscale pt-2' },
   ];
 
   const logos = [sponsorLogosMain, sponsorLogosSecondary, sponsorLogosTertiary];
-  
-  return (
-    <section className="w-full max-w-7xl mx-auto px-4 py-16 min-h-screen flex flex-col items-center justify-center">
-      <TypewriterTitle text="$ ls sponsors/*.svg" className="font-oxanium text-2xl md:text-5xl font-bold mb-12 text-center" />
 
-      <div className="flex flex-col gap-16 py-16 lg:gap-28 lg:py-28">
-        {logos.map((sponsorLogos, index) => (
-          <div className="flex flex-col lg:flex-row flex-wrap items-center justify-center gap-12 lg:gap-32" key={index}>
-          {sponsorLogos.map(({ src, size, alt, url }, sponsorIndex) => (
-            <Tilt
-              key={sponsorIndex}
-              className="parallax-effect"
-              tiltMaxAngleX={10}
-              tiltMaxAngleY={10}
-              perspective={800}
-              transitionSpeed={1500}
-              scale={1.05}
-              gyroscope={true}
-            >
-              <a 
-                href={url}
-                target="_blank"
-                rel="noopener noreferrer"
-                className="w-auto flex items-center justify-center transition-transform duration-300 hover:scale-110"
-              >
-                <Image
-                  src={`/sponsors/${src}`}
-                  alt={alt}
-                  height={size}
-                  width={size}
-                />
-              </a>
-            </Tilt>
-            ))}
-          </div>
-        ))}
-      </div>
+  return (
+    <section className="w-full min-h-screen flex flex-col items-center justify-center pt-24">
+      <Padding>
+        <TypewriterTitle text="$ ls sponsors/*.svg" className="font-oxanium text-2xl md:text-5xl font-bold mb-12 text-center" />
+
+        <div className="flex flex-col gap-16 py-16 lg:gap-28 lg:py-28">
+          {logos.map((sponsorLogos, index) => (
+            <div className="flex flex-col lg:flex-row flex-wrap items-center justify-center gap-12 lg:gap-32" key={index}>
+              {sponsorLogos.map(({ src, size, alt, url, className }, sponsorIndex) => (
+                <Tilt
+                  key={sponsorIndex}
+                  className="parallax-effect"
+                  tiltMaxAngleX={10}
+                  tiltMaxAngleY={10}
+                  perspective={800}
+                  transitionSpeed={1500}
+                  scale={1.05}
+                  gyroscope={true}
+                >
+                  <a
+                    href={url}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="w-auto flex items-center justify-center transition-transform duration-300 hover:scale-110"
+                  >
+                    <Image
+                      src={`/sponsors/${src}`}
+                      alt={alt}
+                      height={size}
+                      width={size}
+                      className={className}
+                    />
+                  </a>
+                </Tilt>
+              ))}
+            </div>
+          ))}
+        </div>
+      </Padding>
     </section>
   );
 };
